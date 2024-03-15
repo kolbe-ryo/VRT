b4d4395623317ccec8b045bc476ffe2952efcf90
kolbe <water.northground@gmail.com>
2024-03-15 16:51:44 +0900

    change ignore

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
     toMatchSnapshot: {
       // An acceptable ratio of pixels that are different to the
       // total amount of pixels, between 0 and 1.
-      maxDiffPixelRatio: 0.1,
+      maxDiffPixelRatio: 0.01,
     },
   },
 
--- tests/search_playwright.spec.ts
+++ tests/search_playwright.spec.ts
@@ -6,6 +6,6 @@ test('test', async ({ page }) => {
   await page.getByLabel('検索', { exact: true }).fill('playwrite');
   await page.getByLabel('検索', { exact: true }).press('Enter');
   await expect(page.locator('#search')).toContainText('Playwright: Fast and reliable end-to-end testing for modern ...');
-  await expect(page).toHaveScreenshot();
+  await expect(await page.screenshot()).toMatchSnapshot();
   await page.close();
 });
