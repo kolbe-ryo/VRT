fbe59b76ced0b573b7753b2dbf4204149be2b1cd
kolbe <water.northground@gmail.com>
2024-03-15 18:15:33 +0900

    change logic

--- tests/search_playwright.spec.ts
+++ tests/search_playwright.spec.ts
@@ -1,10 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/?gws_rd=ssl');
-  await page.getByLabel('検索', { exact: true }).click();
-  await page.getByLabel('検索', { exact: true }).fill('playwrite');
-  await page.getByLabel('検索', { exact: true }).press('Enter');
+  await page.goto('https://www.google.com');
+  // await page.locator('#APjFqb').click();
+  await page.locator('#APjFqb').fill('playwrite');
+  await page.locator('#APjFqb').press('Enter');
+  // await page.getByLabel('検索', { exact: true }).click();
+  // await page.getByLabel('検索', { exact: true }).fill('playwrite');
+  // await page.getByLabel('検索', { exact: true }).press('Enter');
   await expect(page.locator('#search')).toContainText('Playwright: Fast and reliable end-to-end testing for modern ...');
   await expect(await page.screenshot()).toMatchSnapshot();
   await page.close();
